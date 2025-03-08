4f22bd80d1391f0721a745cd3ef67bf915de91a5
LuongHoang <luongkaisa2001@gmail.com>
2025-03-08 19:24:54 +0700

    luong add thêm cái phân trang của tutor đã

---
+++ src/server.js
@@ -0,0 +1,49 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const syncDB = require('./config/syncDB');
+const path = require('path');
+const app = express();
+const cors = require("cors");
+const sessionMiddleware = require("./config/sessionConfig");
+const authRoutes = require("./routes/authRoutes");
+const searchTutorRoutes = require("./routes/searchTutorRoutes");
+const passport = require("passport");
+const authMiddleware = require('./middlewares/authMiddleware');
+const tutorRoutes = require('./routes/adminRoutes')
+require("./config/passport");
+
+
+
+app.use(express.static(path.join(__dirname, '../public')));
+app.use(bodyParser.json());
+app.use(cors({
+    origin: "http://localhost:5173", // Cho phép frontend tại cổng 5173 truy cập 
+    methods: 'GET,POST,PUT,DELETE',
+    credentials: true
+}));
+app.use(express.json()); // Đọc JSON request 
+
+
+//sync database với sequelize models 
+syncDB();
+
+
+//Middleware session
+app.use(sessionMiddleware);
+
+
+//Login Routes
+app.use("/api/auth", authRoutes);
+//app.use("/api/tutor", searchTutorRoutes);
+app.use("/api/tutor", tutorRoutes)
+
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+//learner search routes
+app.use("/learners", authMiddleware, searchTutorRoutes);
+
+
+const PORT = 3001;
+app.listen(PORT, () => console.log(`Server đang chạy tại http://localhost:${PORT}`));
---
+++ src/services/admin/tutorService.js
@@ -0,0 +1,164 @@
+
+const User = require("../../models/User");
+const Tutor = require("../../models/Tutor");
+const Role = require("../../models/Role");
+const Category = require("../../models/Category");
+
+const TutorCategory = require("../../models/TutorCategory");
+const Learner = require("../../models/Learner");
+const TutorTeachLearner = require("../../models/TutorTeachLearner");
+const Contract = require("../../models/Contract");
+
+
+// //Hàm lấy list tutor 
+// exports.getTutors = async () => {
+//     return await Tutor.findAll({
+//         attributes: { exclude: ['verifytoken', 'verified_at', 'tokenexpiry'] }, // Loại bỏ các trường không mong muốn
+//         include: [
+//             {
+//                 model: User,
+//                 attributes: ['userid', 'username', 'displayname', 'email', 'dateofbirth', 'address'],
+//                 include: [
+//                     {
+//                         model: Role,
+//                         attributes: ['rolename'],
+//                         through: { attributes: [] }, // Loại bỏ bảng trung gian UsersRoles khỏi response
+//                     }
+//                 ]
+//             }
+//         ]
+//     });
+// };
+
+// Hàm lấy danh sách tutor có phân trang
+exports.getTutors = async (limit) => {
+    const page = 1; // Mặc định lấy trang đầu tiên
+    const offset = (page - 1) * limit; // Vị trí bắt đầu lấy dữ liệu
+
+    // Lấy tổng số tutor và  tính số trang (đã làm tròn)
+    const totalTutors = await Tutor.count();
+    const totalPages = Math.ceil(totalTutors / limit);
+
+    const tutors = await Tutor.findAll({
+        attributes: { exclude: ['verifytoken', 'verified_at', 'tokenexpiry'] },
+        include: [
+            {
+                model: User,
+                attributes: ['userid', 'username', 'displayname', 'email', 'dateofbirth', 'address'],
+                include: [
+                    {
+                        model: Role,
+                        attributes: ['rolename'],
+                        through: { attributes: [] }
+                    }
+                ]
+            }
+        ],
+        limit,
+        offset
+    });
+
+    return {
+        totalTutors,
+        totalPages,
+        currentPage: page,
+        tutors
+    };
+};
+
+//hàm detail tung tutor
+exports.getTutorDetail = async (userid) => {
+    try {
+        const tutor = await Tutor.findOne({
+            where: { userid },
+            attributes: { exclude: ['verifytoken', 'verified_at', 'tokenexpiry'] }, // Loại bỏ các trường không mong muốn
+            include: [
+                {
+                    model: User,
+                    attributes: ['displayname', 'email', 'imgurl', 'address', 'dateofbirth'] // Lấy thông tin user
+                },
+                {
+                    model: Category,
+                    through: { model: TutorCategory },
+                    attributes: ['categoryname'] // Lấy danh sách môn dạy
+                },
+                {
+                    model: TutorTeachLearner,
+                    include: [
+                        {
+                            model: Learner,
+                            include: {
+                                model: User,
+                                attributes: ['displayname'] // Lấy displayname của learner từ bảng User
+                            }
+                        },
+                        {
+                            model: Contract,
+                            attributes: ['contractid', 'status'] // Lấy trạng thái hợp đồng
+                        }
+                    ]
+                }
+            ]
+        });
+
+        return { success: true, data: tutor };
+    } catch (error) {
+        console.error('Error fetching tutor details:', error);
+        return { success: false, message: 'Internal server error' };
+    }
+};
+
+
+exports.updateTutor = async (userid, updateData) => {
+    try {
+        const {
+            username, imgurl, displayname, email, dateofbirth, address,
+            description, descriptionvideolink, expectedsalary, contracts
+        } = updateData;
+
+        // Cập nhật bảng Users
+        await User.update(
+            { username, displayname, email, dateofbirth, address, imgurl },
+            { where: { userid } }
+        );
+
+        // Cập nhật bảng Tutors
+        await Tutor.update(
+            { description, descriptionvideolink, expectedsalary },
+            { where: { userid } }
+        );
+
+        // Cập nhật trạng thái hợp đồng (Contracts) nếu cần
+        if (contracts && Array.isArray(contracts)) {
+
+            //lặp qua từng hợp đồng trong mảng contracts
+            for (const contract of contracts) {
+                await Contract.update(
+                    { status: contract.status },
+
+                    // Điều kiện: cập nhật bản ghi có contractid trùng với contractid được truyền vào
+                    { where: { contractid: contract.contractid } }
+                );
+            }
+        }
+
+        // Lấy thông tin mới cập nhật
+        const updatedTutor = await Tutor.findOne({
+            where: { userid },
+            attributes: { exclude: ['verifytoken', 'verified_at', 'tokenexpiry'] }, // Loại bỏ các trường không mong muốn
+            include: [
+                { model: User, attributes: ['username', 'displayname', 'email', 'dateofbirth', 'address', 'imgurl'] },
+                {
+                    model: TutorTeachLearner,
+                    include: [
+                        { model: Contract, attributes: ['contractid', 'status'] }
+                    ]
+                }
+            ]
+        });
+
+        return updatedTutor;
+    } catch (error) {
+        throw new Error('Error updating tutor: ' + error.message);
+    }
+};
