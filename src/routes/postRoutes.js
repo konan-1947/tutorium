08aa714ad36f5b8590ccd3028104ff5df1ec280e
Nhat Mingg <minhvu2908b@gmail.com>
2025-04-13 00:20:18 +0700

    Add files via upload

--- src/routes/postRoutes.js
+++ src/routes/postRoutes.js
@@ -3,6 +3,7 @@ const router = express.Router();
 const postController = require('../controllers/post/postController');
 const { upload } = require('../middlewares/uploadMiddleware');
 //const { deleteImage } = require('../controllers/postController');
+const savedPostController = require('../controllers/post/savedPostController');
 const PostImage = require('../models/PostImage');
 const multer = require("multer");
 
@@ -12,6 +13,9 @@ const storage = multer.memoryStorage();
 // const upload = multer({ storage: storage });
 // Lấy tất cả bài viết
 router.get('/', postController.getAllPosts);
+
+// View saved posts
+router.get('/ditmesaveloivcl', savedPostController.getSavedPostsJSON);
 
 router.get('/myposts', postController.getMyPosts);
 
@@ -24,8 +28,7 @@ router.delete('/delete-image/:imageid',  postController.deleteImage);
 router.get('/getUser/:userid',postController.getUserProfile);
 
 //Lấy tên người dùng qua session
-// lấy post theo id
-router.get("/:postid", postController.getPostById);
+
 
 // Tạo Google Docs mới
 router.post("/create-google-doc", postController.createGoogleDoc);
@@ -34,7 +37,16 @@ router.post("/create-google-doc", postController.createGoogleDoc);
 router.delete('/delete/:postid', postController.deletePost);
 
 router.post('/toggleUpvote', postController.toggleUpvote);
+
 router.get('/getUpvoteCount/:postid', postController.getUpvoteCount);
 
+// Save a post
+router.post('/save/:postid', savedPostController.savePost);
+
+// lấy post theo id
+router.get("/:postid", postController.getPostById);
+
+
+router.delete('/saved/:postid', savedPostController.unsavePost);
 
 module.exports = router;
